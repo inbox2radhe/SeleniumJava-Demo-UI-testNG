3ff185495d8b26f7076e4e1762920a97905bdbff
Giribabu Kristamsetty (OSV) <giribabu.kristamsetty.osv@fedex.com>
2022-05-16 01:21:18 +0000

    Update BaseClass.java

--- src/main/java/com/NonKeyword_FutureReadyFramework/automation/testBase/BaseClass.java
+++ src/main/java/com/NonKeyword_FutureReadyFramework/automation/testBase/BaseClass.java
@@ -27,7 +27,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.NonKeyword_FutureReadyFramework.automation.excelReader.ExcelUtils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+// import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	public static WebDriver driver;
