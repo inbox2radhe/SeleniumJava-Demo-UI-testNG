3477fb215f9c3b91d2dd239772643da4c104d5ba
Radhe Singh (OSV) <radhe.singh.osv@fedex.com>
2022-06-02 15:25:06 +0000

    Upload New File

---
+++ src/test/java/com/NonKeyword_FutureReadyFramework/automation/testCases/TC13.java
@@ -0,0 +1,32 @@
+package com.NonKeyword_FutureReadyFramework.automation.testCases;
+
+import java.io.IOException;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.NonKeyword_FutureReadyFramework.automation.application.LoginPage;
+import com.NonKeyword_FutureReadyFramework.automation.customListener.ListenTest;
+import com.NonKeyword_FutureReadyFramework.automation.excelReader.ExcelUtils;
+import com.NonKeyword_FutureReadyFramework.automation.testBase.BaseClass;
+
+
+	
+	@Listeners(ListenTest.class)
+	public class TC13 extends BaseClass {
+
+		@Test(enabled = true)
+		public void magentoExecution() throws IOException, InterruptedException {
+
+			LoginPage loginPage = new LoginPage();
+			ExcelUtils excelRead = new ExcelUtils();
+			int rowNumber = excelRead.excelReadTestCaseRowNo(getClass().getSimpleName());
+			System.out.print("TC Row number:"+rowNumber);
+			
+			loginPage.loginFuction_DMT(rowNumber);
+
+		}
+
+	}
+
+
